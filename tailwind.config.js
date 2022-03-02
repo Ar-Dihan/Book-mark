590b4be68509a8cca9e7e097cb8405631a470e66
Ar-Dihan <arnahid18@gmail.com>
2022-03-02 16:05:41 +0600

    half done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bookMark-purple': '#5267D5',
+        'bookMark-red': '#FA5959',
+        'bookMark-blue': '#242A45',
+        'bookMark-gray': '#9194A2',
+        'bookMark-white': '#F7F7F7',
+      },
+    },
+    fontFamily: {
+      Poppins: ['Poppins', 'sans-serif'],
+    },
+    container:{
+      center: true,
+      padding: '2rem',
+      screens:{
+        lg: '1124',
+        xl: '1124',
+        '2xl': '1124',
+      },
+    },
   },
   plugins: [],
 }
